c1cb04d7ad0cb838ac29b491d835d4c153de4b05
Mirko Stocker <me@misto.ch>
2017-09-26 12:58:45 +0200

    solution

--- app/src/main/java/ch/hsr/mge/gadgeothek/ui/GadgeothekActivity.java
+++ app/src/main/java/ch/hsr/mge/gadgeothek/ui/GadgeothekActivity.java
@@ -5,6 +5,8 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import ch.hsr.mge.gadgeothek.R;
@@ -46,6 +48,41 @@ public class GadgeothekActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.gadgeothek, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                LibraryService.logout(new Callback<Boolean>() {
+                    @Override
+                    public void onCompletion(Boolean input) {
+                        SharedPreferences preferences = getSharedPreferences(AbstractAuthenticationActivity.PREFERENCES, MODE_PRIVATE);
+                        SharedPreferences.Editor editor = preferences.edit();
+                        // disable auto-login for the next time
+                        editor.putBoolean(AbstractAuthenticationActivity.LAST_AUTOLOGIN_FAILED, true);
+                        editor.apply();
+                        Intent intent = new Intent(GadgeothekActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        overridePendingTransition(R.anim.slide_in_left_to_right, R.anim.slide_out_left_to_right);
+                        finish();
+                    }
+
+                    @Override
+                    public void onError(String message) {
+
+                    }
+                });
+                return true;
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public Toolbar getToolbar() {
